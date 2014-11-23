97d5f475c5ab59fcaa1d56ee2b6c7f49dd8e7849
ras <ras@RAS26.(none)>
2014-11-23 19:19:20 +0100

    Publishing transform from world to robot

--- src/odometry.cpp
+++ src/odometry.cpp
@@ -1,8 +1,11 @@
 #include "ros/ros.h"
 #include "ras_arduino_msgs/Encoders.h"
 #include "geometry_msgs/Pose2D.h"
+#include <visualization_msgs/MarkerArray.h>
 #include "sensor_msgs/Imu.h"
 #include "ras_utils/ras_utils.h"
+#include <tf/transform_broadcaster.h>
+
 #define PUBLISH_RATE 50 // Hz
 #define QUEUE_SIZE 1000
 
@@ -26,13 +29,18 @@ private:
     ros::NodeHandle n_;
 
     // Publishes geometry_msgs message to topic: robot/pose2d with x, y coordinates and CONTINUOUS angle.
-    ros::Publisher pose2d_pub_;
+    ros::Publisher pose2d_pub_, marker_pub_;
     ros::Subscriber encoder_sub_;
     ros::Subscriber imu_sub_;
+    static tf::TransformBroadcaster br;
 
     // Callback func when encoder data received
     void encodersCallback(const ras_arduino_msgs::Encoders::ConstPtr& msg);
     void IMUCallback(const sensor_msgs::Imu::ConstPtr& msg);
+
+    void publish_transform(double x, double y, double theta);
+
+    void publish_marker();
 
     ros::WallTime t_IMU;
     bool IMU_init_;
@@ -61,6 +69,7 @@ Odometric_coordinates::Odometric_coordinates(const ros::NodeHandle &n)
 
     // Publisher
     pose2d_pub_ = n_.advertise<geometry_msgs::Pose2D>(TOPIC_ODOMETRY, QUEUE_SIZE);
+    marker_pub_ = n_.advertise<visualization_msgs::MarkerArray>(TOPIC_MARKERS, QUEUE_SIZE);
     // Subscriber
     encoder_sub_ = n_.subscribe("/arduino/encoders", QUEUE_SIZE,  &Odometric_coordinates::encodersCallback, this);
     // imu_sub_ = n_.subscribe("/imu/data", QUEUE_SIZE,  &Odometric_coordinates::IMUCallback, this);
@@ -107,13 +116,10 @@ void Odometric_coordinates::run()
         msg.y = y;
         msg.theta = angle;
 
-        // print
-        ROS_INFO("[Odometry] X: %.3f Y: %.3f Theta: %.3f", x,y,angle);
-//        std::cout << "delta_x: " << delta_x << " delta_y: " << delta_y << " delta_theta: " << delta_angle << std::endl;
-//        std::cout << "x: " << x << " y: " << y << " theta: " << angle << "\n" << std::endl;
-
         pose2d_pub_.publish(msg);
+        publish_transform(x,y,angle);
 
+        publish_marker();
         // ** Sleep
         ros::spinOnce();
         loop_rate.sleep();
@@ -122,3 +128,73 @@ void Odometric_coordinates::run()
     std::cout << "Exiting...\n";
 }
 
+void Odometric_coordinates::publish_transform(double x, double y, double theta)
+{
+    static tf::TransformBroadcaster br;
+    tf::Transform transform;
+    transform.setOrigin( tf::Vector3(x, y, 0.0) );
+    tf::Quaternion q;
+    q.setRPY(0, 0, theta);
+    transform.setRotation(q);
+    br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), COORD_FRAME_WORLD, COORD_FRAME_ROBOT));
+}
+
+void Odometric_coordinates::publish_marker()
+{
+    visualization_msgs::MarkerArray msg;
+    msg.markers.resize(2); // Print object position and arrow
+
+    visualization_msgs::Marker & marker_obj = msg.markers[0];
+    visualization_msgs::Marker & marker_arrow = msg.markers[1];
+
+    // Robot
+    marker_obj.header.frame_id = COORD_FRAME_WORLD;
+    marker_obj.header.stamp = ros::Time();
+    marker_obj.ns = "Robot";
+    marker_obj.id = 0;
+    marker_obj.action = visualization_msgs::Marker::ADD;
+    marker_obj.pose.position.x = x;
+    marker_obj.pose.position.y = y;
+    marker_obj.pose.position.z = 0;
+
+    marker_obj.pose.orientation.x = 0.0;
+    marker_obj.pose.orientation.y = 0.0;
+    marker_obj.pose.orientation.z = angle;
+    marker_obj.pose.orientation.w = 1.0;
+    marker_obj.scale.x = 0.1;
+    marker_obj.scale.y = 0.1;
+    marker_obj.scale.z = 0.1;
+
+    marker_obj.color.a = 1.0;
+    marker_obj.color.r = 1.0;
+    marker_obj.color.g = 0.0;
+    marker_obj.color.b = 0.0;
+    marker_obj.type = visualization_msgs::Marker::SPHERE;
+
+    // Arrow
+    marker_arrow.header.frame_id = COORD_FRAME_WORLD;
+    marker_arrow.header.stamp = ros::Time();
+    marker_arrow.ns = "Robot";
+    marker_arrow.id = 1;
+    marker_arrow.action = visualization_msgs::Marker::ADD;
+    marker_arrow.pose.position.x = x;
+    marker_arrow.pose.position.y = y;
+    marker_arrow.pose.position.z = 0;
+
+    marker_arrow.pose.orientation.x = 0.0;
+    marker_arrow.pose.orientation.y = 0.0;
+    marker_arrow.pose.orientation.z = angle;
+    marker_arrow.pose.orientation.w = 1.0;
+    marker_arrow.scale.x = 0.2;
+    marker_arrow.scale.y = 0.02;
+    marker_arrow.scale.z = 0.02;
+
+    marker_arrow.color.a = 1.0;
+    marker_arrow.color.r = 0.0;
+    marker_arrow.color.g = 0.0;
+    marker_arrow.color.b = 1.0;
+    marker_arrow.type = visualization_msgs::Marker::ARROW;
+
+    marker_pub_.publish( msg);
+
+}
