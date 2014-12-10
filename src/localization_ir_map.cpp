605b87b4fbd1fb4313d9b7075e7370fa6dcb6423
Carlos Galvez <cgdpf@kth.se>
2014-12-10 12:49:28 +0100

    Localization coded, NOT TESTD

--- src/localization_ir_map.cpp
+++ src/localization_ir_map.cpp
@@ -9,29 +9,25 @@ Localization_IR_Map::Localization_IR_Map(const nav_msgs::OccupancyGrid::ConstPtr
     this->map_ = map;
 }
 
-void Localization_IR_Map::updatePose(const ras_arduino_msgs::ADConverter::ConstPtr &adc_msg,
+void Localization_IR_Map::updatePose(const ras_srv_msgs::IRData::ConstPtr &adc_msg,
                                      const geometry_msgs::Pose2D::ConstPtr &current_pose,
                                      geometry_msgs::Pose2D::Ptr &updated_pose)
-{
-    // ** Transform ADC into meters
-    ADC_Data_M adc_data;
-    this->constructADCData(adc_msg, adc_data);
-
+{    
     // ** Update theta
-    double newTheta = this->updateTheta(adc_data, current_pose->theta);
+    double newTheta = this->updateTheta(adc_msg, current_pose->theta);
 
     // ** Update pose
-    this->updateXY(current_pose, adc_data, this->map_, newTheta, updated_pose);
+    this->updateXY(current_pose, adc_msg, this->map_, newTheta, updated_pose);
 }
 
-double Localization_IR_Map::updateTheta(const ADC_Data_M &adc_data, double currentTheta)
+double Localization_IR_Map::updateTheta(const ras_srv_msgs::IRData::ConstPtr &adc_data, double currentTheta)
 {
     // ** Find closest wall
-    double d_right = 0.5*(adc_data.front_right_ + adc_data.back_right_);
-    double d_left  = 0.5*(adc_data.front_left_  + adc_data.back_left_);
+    double d_right = 0.5*(adc_data->front_right + adc_data->back_right);
+    double d_left  = 0.5*(adc_data->front_left  + adc_data->back_left);
 
-    double d1 = d_right < d_left ? adc_data.back_right_ : adc_data.front_left_;
-    double d2 = d_right < d_left ? adc_data.front_right_: adc_data.back_left_;
+    double d1 = d_right < d_left ? adc_data->back_right : adc_data->front_left;
+    double d2 = d_right < d_left ? adc_data->front_right: adc_data->back_left;
 
     // ** Get theta0
     std::vector<double> theta0v = {-M_PI, -M_PI/2.0, 0.0, M_PI/2.0, M_PI};
@@ -54,7 +50,8 @@ double Localization_IR_Map::updateTheta(const ADC_Data_M &adc_data, double curre
 }
 
 void Localization_IR_Map::updateXY(const geometry_msgs::Pose2D::ConstPtr &currentPose,
-                                   const ADC_Data_M &adc_data, const nav_msgs::OccupancyGrid::ConstPtr &map,
+                                   const ras_srv_msgs::IRData::ConstPtr &adc_data,
+                                   const nav_msgs::OccupancyGrid::ConstPtr &map,
                                    double updatedTheta, geometry_msgs::Pose2D::Ptr &updatedPose)
 {
     double x_optimal, y_optimal, min_cost = std::numeric_limits<double>::infinity();
@@ -83,42 +80,111 @@ void Localization_IR_Map::updateXY(const geometry_msgs::Pose2D::ConstPtr &curren
     updatedPose->theta = updatedTheta;
 }
 
-double Localization_IR_Map::computeCost(const ADC_Data_M &adc_data,
-                                      const nav_msgs::OccupancyGrid::ConstPtr &map,
-                                      const geometry_msgs::Pose2D &test_pose)
+double Localization_IR_Map::computeCost(const ras_srv_msgs::IRData::ConstPtr &adc_data,
+                                        const nav_msgs::OccupancyGrid::ConstPtr &map,
+                                        const geometry_msgs::Pose2D &test_pose)
 {
-    ADC_Data_M raytraced_data;
+    ras_srv_msgs::IRData raytraced_data;
     raytrace(test_pose, map, raytraced_data);
 
-    double cost  = pow(adc_data.back_        - raytraced_data.back_,        2) +
-                   pow(adc_data.back_left_   - raytraced_data.back_left_,   2) +
-                   pow(adc_data.back_right_  - raytraced_data.back_right_,  2) +
-                   pow(adc_data.front_       - raytraced_data.front_,       2) +
-                   pow(adc_data.front_left_  - raytraced_data.front_left_,  2) +
-                   pow(adc_data.front_right_ - raytraced_data.front_right_, 2);
+    double cost  = pow(adc_data->back        - raytraced_data.back,        2) +
+                   pow(adc_data->back_left   - raytraced_data.back_left,   2) +
+                   pow(adc_data->back_right  - raytraced_data.back_right,  2) +
+                   pow(adc_data->front       - raytraced_data.front,       2) +
+                   pow(adc_data->front_left  - raytraced_data.front_left,  2) +
+                   pow(adc_data->front_right - raytraced_data.front_right, 2);
 
     return cost;
 }
 
 void Localization_IR_Map::raytrace(const geometry_msgs::Pose2D &test_pose,
                                    const nav_msgs::OccupancyGrid::ConstPtr &map,
-                                   ADC_Data_M &raytraced_data)
+                                   ras_srv_msgs::IRData &raytraced_data)
 {
-    ROS_ERROR("[Localization_IR_Map::raytrace] TO DO");
+    raytraced_data.front_right = raytraceSensor(test_pose, IR_SENSOR_FRONT_RIGHT, *map);
+    raytraced_data.front_left  = raytraceSensor(test_pose, IR_SENSOR_FRONT_LEFT, *map);
+    raytraced_data.back_right  = raytraceSensor(test_pose, IR_SENSOR_BACK_RIGHT, *map);
+    raytraced_data.back_left   = raytraceSensor(test_pose, IR_SENSOR_BACK_LEFT, *map);
+    raytraced_data.front       = raytraceSensor(test_pose, IR_SENSOR_FRONT, *map);
+    raytraced_data.back        = raytraceSensor(test_pose, IR_SENSOR_BACK, *map);
 }
 
-void Localization_IR_Map::constructADCData(const ras_arduino_msgs::ADConverter::ConstPtr &adc_msg,
-                                           ADC_Data_M &adc_data)
+double Localization_IR_Map::raytraceSensor(const geometry_msgs::Pose2D &test_pose, int sensor_id,
+                                         const nav_msgs::OccupancyGrid & occ_grid)
 {
-    // ** CHECK THAT THE CHANNELS ARE THE ONES THAT ARE SUPPOSED TO BE!!
-    double d_front_right = RAS_Utils::sensors::shortSensorToDistanceInCM(adc_msg->ch1) * 0.01;
-    double d_back_right  = RAS_Utils::sensors::shortSensorToDistanceInCM(adc_msg->ch2) * 0.01;
-    double d_back_left   = RAS_Utils::sensors::shortSensorToDistanceInCM(adc_msg->ch3) * 0.01;
-    double d_front_left  = RAS_Utils::sensors::shortSensorToDistanceInCM(adc_msg->ch4) * 0.01;
+    double sensor_x_pos, sensor_y_pos, sensor_angle;
+    computeSensorXYThetaWorld(test_pose, sensor_id, sensor_x_pos, sensor_y_pos, sensor_angle);
 
-    double d_front       = RAS_Utils::sensors::longSensorToDistanceInCM(adc_msg->ch7) * 0.01;
-    double d_back        = RAS_Utils::sensors::longSensorToDistanceInCM(adc_msg->ch8) * 0.01;
-
-    adc_data = ADC_Data_M(d_front_right, d_back_right, d_front_left, d_back_left, d_front, d_back);
+    double stepper = 0.1;
+    for(double current_distance = stepper; current_distance < 30; current_distance += stepper)
+    {
+        double new_x_pos = sensor_x_pos + cos(sensor_angle) * current_distance;
+        double new_y_pos = sensor_y_pos + sin(sensor_angle) * current_distance;
+        bool pos_is_wall = RAS_Utils::occ_grid::isWall(occ_grid, new_x_pos, new_y_pos);
+        if(pos_is_wall)
+        {
+            return current_distance;
+        }
+    }
+    return -1;
 }
 
+void Localization_IR_Map::computeSensorXYThetaWorld(const geometry_msgs::Pose2D &robot_pose, int sensor_id,
+                                 double & sensor_x_pos, double & sensor_y_pos, double & sensor_theta)
+{
+    tf::Transform tf_robot_to_world;
+    tf::Quaternion q;
+    q.setRPY(0,0,robot_pose.theta);
+    tf::Vector3 origin(robot_pose.x, robot_pose.y, 0.0);
+
+    tf_robot_to_world.setOrigin(origin);
+    tf_robot_to_world.setRotation(q);
+
+    Eigen::Matrix4f tf_eigen;
+    PCL_Utils::convertTransformToEigen4x4(tf_robot_to_world, tf_eigen);
+
+    pcl::PointXYZ p;
+    p.z = 0.0;
+    switch (sensor_id)
+    {
+    case IR_SENSOR_FRONT_RIGHT:
+        p.x = SHORT_SENSOR_OFFSET_X;
+        p.y = -SHORT_SENSOR_OFFSET_Y;
+        sensor_theta = RAS_Utils::normalize_angle(robot_pose.theta - M_PI/2.0);
+        break;
+
+    case IR_SENSOR_BACK_RIGHT:
+        p.x = -SHORT_SENSOR_OFFSET_X;
+        p.y = -SHORT_SENSOR_OFFSET_Y;
+        sensor_theta = RAS_Utils::normalize_angle(robot_pose.theta - M_PI/2.0);
+        break;
+
+    case IR_SENSOR_FRONT_LEFT:
+        p.x = SHORT_SENSOR_OFFSET_X;
+        p.y = SHORT_SENSOR_OFFSET_Y;
+        sensor_theta = RAS_Utils::normalize_angle(robot_pose.theta + M_PI/2.0);
+        break;
+
+
+    case IR_SENSOR_BACK_LEFT:
+        p.x = -SHORT_SENSOR_OFFSET_X;
+        p.y = SHORT_SENSOR_OFFSET_Y;
+        sensor_theta = RAS_Utils::normalize_angle(robot_pose.theta + M_PI/2.0);
+        break;
+
+    case IR_SENSOR_FRONT:
+        p.x = LONG_SENSOR_OFFSET_X;
+        p.y = 0.0;
+        sensor_theta = robot_pose.theta;
+        break;
+
+    case IR_SENSOR_BACK:
+        p.x = -LONG_SENSOR_OFFSET_X;
+        p.y =  0.0;
+        sensor_theta = -robot_pose.theta;
+        break;
+    }
+    PCL_Utils::transformPoint(p, tf_eigen, p);
+    sensor_x_pos = p.x;
+    sensor_y_pos = p.y;
+}
